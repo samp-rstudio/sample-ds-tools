918f1569ae3726d2b6b3bb6d3501189351c9163a
Sam Perman <sam@posit.co>
2024-01-29 09:50:38 -0500

    add a library

--- shinypython/app.py
+++ shinypython/app.py
@@ -1,12 +1,14 @@
 import platform
 import subprocess
-
+from humanfriendly import format_size
 from shiny import App, render, ui
 
 app_ui = ui.page_fluid(
     ui.h2("Environment stuff!"),
     ui.span("Python"),
     ui.output_text_verbatim("python"),
+    ui.span("Disk free (hardcoded for now)"),
+    ui.output_text_verbatim("diskfree"),
     ui.input_text_area("cmd", "Command to run", placeholder="Enter text"),
     ui.output_text_verbatim("cmd_output"),
 )
@@ -17,6 +19,11 @@ def server(input, output, session):
     @render.text
     def python():
         return platform.python_version()
+
+    @output
+    @render.text
+    def diskfree():
+        return format_size(16000000000)
 
     @output
     @render.text
